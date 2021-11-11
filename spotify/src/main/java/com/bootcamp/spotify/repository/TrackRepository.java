6ac84000e6e0e14bf94e1ee24e9e2227f0f21d9a
belen yammal <belentammal@gmail.com>
2021-11-11 12:07:57 -0300

    mapeo de identidades realizada

---
+++ spotify/src/main/java/com/bootcamp/spotify/repository/TrackRepository.java
@@ -0,0 +1,10 @@
+package com.bootcamp.spotify.repository;
+
+import com.bootcamp.spotify.domain.model.Track;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends CrudRepository<Track, Long> {
+
+}
